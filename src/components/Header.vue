dfcead19b04f5425428fbfd6c390cd2a4c8d6eaf
Jeremy McAlpine <jeremy@dropil.com>
2020-04-02 17:02:28 -0700

    Fix Zeli wallet link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@
       </div>
 
       <ul class="mininav">        
-        <li><a href="https://zeli.io">Zeli Wallet</a></li>
+        <li><a href="https://zeli.dropilchain.com">Zeli Wallet</a></li>
         <li><a href="https://docs.dropilchain.com">Docs <i class="far fa-external-link"></i></a></li>
         <li><a href="https://dropilchain.com">Website <i class="far fa-external-link"></i></a></li>
       </ul>
